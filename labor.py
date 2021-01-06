d907b4ac86df688d809c0839b76b34850711ab21
Kelven Opoku <kelvenopoku@gmail.com>
2021-01-06 13:20:53 -0500

    added input validation

--- labor.py
+++ labor.py
@@ -4,14 +4,17 @@ from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
+import pyinputplus as pyip
 
-driver = webdriver.Chrome(executable_path="/path_to_chromedriver")
+driver = webdriver.Chrome(executable_path="/Users/kelvenopoku/Downloads/chromedriver")
+# driver = webdriver.Chrome(executable_path="/path_to_chromedrive")
 profile = {}
 
 def main():
+	
 	profile = get_user_info()
 	login(profile['username'], profile['password'])	
-
+	
 	if navigate() == False:
 		print("There is an issue getting to your benefit claim page. Have you already claimed benefits for this week?")
 	else: 
@@ -27,7 +30,7 @@ def login(username, password):
 	driver.find_element_by_id("loginform:username").send_keys(username)
 	driver.find_element_by_id("loginform:password").send_keys(password)
 	
-	#User will fill Recaptcha button and click submit??
+	#Recaptcha
 	time.sleep(75)
 
 	driver.find_element_by_id("loginform:signinButton").click()
@@ -77,7 +80,6 @@ def fill():
 			EC.presence_of_element_located((By.ID, "G05_DAYS_NOT_RWA")
 		))
 	
-
 		Select(element).select_by_value(profile['Q3'])
 
 		#Question 4: How many days were you owed vacation pay or did you receive vacation pay?
@@ -94,54 +96,41 @@ def fill():
 	except:
 		print("Failed")
 	#Question 1:  During the week ending 8/23/2020, did you refuse any job offer or referral?
-	if profile['Q1'].upper() == "N":
+	if profile['Q1'] == "No":
 		driver.find_element_by_id("G05_REFUSE_OFFER0").click()
-	elif profile['Q1'].upper() == "Y": 
+	elif profile['Q1'] == "Yes": 
 		driver.find_element_by_id("G05_REFUSE_OFFER1").click()
-	else:
-		raise Exception("Please enter 'Y' or 'N'")
 
 	#Question 2a: Excluding earnings from self-employment, did you earn more than $504?
-	if profile['Q2a'].upper() == "NA":
+	if profile['Q2a'] == "NA":
 		driver.find_element_by_id("G05_EXCEEDED_MAX_EARNINGS3").click()
-	elif profile['Q2a'].upper() == "Y": 
+	elif profile['Q2a'] == "Yes": 
 		driver.find_element_by_id("G05_EXCEEDED_MAX_EARNINGS1").click()
-	elif profile['Q2a'].upper() == "N":
+	elif profile['Q2a'] == "No":
 		driver.find_element_by_id("G05_EXCEEDED_MAX_EARNINGS0").click()
-	else:
-		raise Exception("Please enter 'Y' or 'N'")
-
-	#Question 6: Have you retuned to work?
-	if profile['Q6'].upper() == "N":
-		driver.find_element_by_id("G05_RETURNED_FULL_TIME0").click()
-	elif profile['Q6'].upper() == "Y": 
-		driver.find_element_by_id("G05_RETURNED_FULL_TIME1").click()
-	else:
-		raise Exception("Please enter 'Y' or 'N'")
 	
-	#when the confimation page is detected, quit the driver
-	# try:
-	# 	WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//*[@id='main']/div/div/div[4]/div/div/div/li")))
-	# except:
-	# 	print("Cannot locate confirmation page")
-	# driver.quit()
+	#Question 6: Have you retuned to work?
+	if profile['Q6'] == "No":
+		driver.find_element_by_id("G05_RETURNED_FULL_TIME0").click()
+	elif profile['Q6'] == "Yes": 
+		driver.find_element_by_id("G05_RETURNED_FULL_TIME1").click()
+	
 
 def get_user_info():
 	
-	profile['username'] = input("Enter Username: ")
-	profile['password'] = input("Enter Password: ")
-
-	profile['Q1'] = input("Last week, did you refuse any job offer or referral? (Y/N) ")
-	profile['Q2'] = input("How many days did you work, including self-employment, during the week ending 8/23/2020? (Enter '4' if 4 or more) ")
-	profile['Q2a'] = input("Excluding earnings from self-employment, did you earn more than $504? (Y/N/NA) ")
-	profile['Q3'] = input("How many days were you NOT ready, willing, and able to work? (Enter '4' if 4 or more) ")
-	profile['Q4'] = input("How many days were you owed vacation pay or did you receive vacation pay? (Enter '4' if 4 or more) ")
-	profile['Q5'] = input("How many days were you owed holiday pay or did you receive holiday pay? (Enter '4' if 4 or more) ")
-	profile['Q6'] = input("Have you retuned to work? (Y/N) ")
+	profile['username'] = pyip.inputStr("Enter Username: ")
+	profile['password'] = pyip.inputStr("Enter Password: ")
+	
+	profile['Q1'] = pyip.inputChoice(['Yes', 'No'], prompt="Last week, did you refuse any job offer or referral? ('Yes'/'No') ")
+	profile['Q2'] = pyip.inputStr("How many days did you work, including self-employment, in the last week? (Enter '4' if 4 or more) ")
+	profile['Q2a'] = pyip.inputChoice(['Yes', 'No', 'NA'], prompt="Excluding earnings from self-employment, did you earn more than $504? ('Yes'/'No'/'NA') ")
+	profile['Q3'] = pyip.inputStr("How many days were you NOT ready, willing, and able to work? (Enter '4' if 4 or more) ")
+	profile['Q4'] = pyip.inputStr("How many days were you owed vacation pay or did you receive vacation pay? (Enter '4' if 4 or more) ")
+	profile['Q5'] = pyip.inputStr("How many days were you owed holiday pay or did you receive holiday pay? (Enter '4' if 4 or more) ")
+	profile['Q6'] = pyip.inputChoice(['Yes', 'No'], prompt="Have you retuned to work? ('Yes'/'No') ")
+	
 
 	return profile
-
-
 
 
 main()
